2a545ece62498860adbe1dee5e8f8c93a654e6ce
simtape <s.cassetta1@studenti.unimol.it>
2021-10-17 20:03:38 +0200

    fixed RowDestroyer, now it takes all the project to analyze. Firstly it checks if the projects are in Java, and if this is verified, the plugins proceeds to mutate a random java file of the project

--- src/it/unimol/com/Main.java
+++ src/it/unimol/com/Main.java
@@ -1,16 +1,12 @@
 package it.unimol.com;
-
 import it.unimol.com.gui.Board;
-
 public class Main {
     public static void main (String args[]){
         Handler handler = new Handler();
         handler.createMaze();
         handler.showMaze();
-
-        Board board = new Board(handler.getMazeBFS(), handler.getMazeDFS(), handler.getMazeNotSolved());
+        Board board = new Board(handler.getMazeBFS(), handler.getMazeDFS(), handler.getMazeNotSolved())
         board.setSize(600, 600);
         board.setVisible(true);
-
     }
 }
